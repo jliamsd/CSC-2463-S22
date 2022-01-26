2b6964426f05ce2952ae6e171541630e651b0771
Liam Sheehan-Dean <60580605+jliamsd@users.noreply.github.com>
2022-01-25 18:12:00 -0600

    Create sketch.js

---
+++ HW1/2/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  noStroke();
+  
+  fill('rgba(255,0,0,0.25)');
+  circle(200,125,200);
+  fill('rgba(0,255,0,0.25)');
+  circle(266,225,200);
+  fill('rgba(0,0,255,0.25)');
+  circle(133,225,200);
+}
